daaad19d2086978bff406d46dae3626906e97d9a
Anuraj <anuraj.p@msn.com>
2018-11-09 19:55:35 +0530

    Authentication implementation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FeedReader.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
 
 namespace FeedReader.Controllers
 {
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +13,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 
 namespace FeedReader
 {
@@ -48,6 +53,43 @@ namespace FeedReader
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
+                {
+                    var azureAppServicePrincipalIdHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"][0];
+                    var azureAppServicePrincipalNameHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"][0];
+                    var cookieContainer = new CookieContainer();
+                    HttpClientHandler handler = new HttpClientHandler()
+                    {
+                        CookieContainer = cookieContainer
+                    };
+                    string uriString = $"{context.Request.Scheme}://{context.Request.Host}";
+                    foreach (var c in context.Request.Cookies)
+                    {
+                        cookieContainer.Add(new Uri(uriString), new Cookie(c.Key, c.Value));
+                    }
+                    string jsonResult = string.Empty;
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        var res = await client.GetAsync($"{uriString}/.auth/me");
+                        jsonResult = await res.Content.ReadAsStringAsync();
+                    }
+                    var obj = JArray.Parse(jsonResult);
+                    string user_id = obj[0]["user_id"].Value<string>();
+                    List<Claim> claims = new List<Claim>();
+                    foreach (var claim in obj[0]["user_claims"])
+                    {
+                        claims.Add(new Claim(claim["typ"].ToString(), claim["val"].ToString()));
+                    }
+                    var identity = new GenericIdentity(azureAppServicePrincipalIdHeader);
+                    identity.AddClaims(claims);
+                    context.User = new GenericPrincipal(identity, null);
+                };
+
+                await next.Invoke();
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
